f5f374f42c391ec81083d12dd50e1490a2c8bfe7
Kimentii <malafeikirill@mail.ru>
2017-09-29 19:18:16 +0300

    Добавлен вывод значения заряда.

--- WorkWithBattery(lab3)/app/src/main/java/com/example/kimentii/workwithbatterylab3/MainActivity.java
+++ WorkWithBattery(lab3)/app/src/main/java/com/example/kimentii/workwithbatterylab3/MainActivity.java
@@ -1,16 +1,22 @@
 package com.example.kimentii.workwithbatterylab3;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     Button createService;
     Button destroyService;
+    TextView textView;
 
     public class Listener implements View.OnClickListener {
 
@@ -37,6 +43,16 @@ public class MainActivity extends AppCompatActivity {
         Listener listener = new Listener();
         createService.setOnClickListener(listener);
         destroyService.setOnClickListener(listener);
+        textView = (TextView) findViewById(R.id.battary);
+        BroadcastReceiver receiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                int status = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+                textView.setText(String.valueOf(status));
+            }
+        };
+
+        registerReceiver(receiver, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
         Log.d("myTag", "Application was started");
     }
 }
