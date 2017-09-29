664eac24f4b7412982750512c84f4e7f9a7eeacf
Kimentii <malafeikirill@mail.ru>
2017-09-29 10:49:49 +0300

    Сделан вывод версии прошивки, версии шины, серийного номера.

---
+++ WorkWithHDD(lab2)/WorkWithHDD(lab2)/Source.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <Windows.h>
+#include <WinIoCtl.h>
+#include <ntddscsi.h>
+#include <conio.h>
+
+using namespace std;
+
+#define bThousand 1024
+
+char* busType[] = { "UNKNOWN", "SCSI", "ATAPI", "ATA", "ONE_TREE_NINE_FOUR", "SSA", "FIBRE", "USB", "RAID", "ISCSI", "SAS", "SATA", "SD", "MMC" };
+
+void getDeviceInfo(HANDLE diskHandle, STORAGE_PROPERTY_QUERY storageProtertyQuery) {
+
+	//Выделение памяти под дескриптор устройства
+	STORAGE_DEVICE_DESCRIPTOR* deviceDescriptor = (STORAGE_DEVICE_DESCRIPTOR*)calloc(bThousand, 1);
+
+	deviceDescriptor->Size = bThousand;
+
+	//Отправляем управляющий код непосредственно указанному драйверу устройства, заставляя устройство выполнить соответствующую операцию.
+	if (!DeviceIoControl(diskHandle,
+		IOCTL_STORAGE_QUERY_PROPERTY,				//Отправляем запрос на возврат свойств устройства.
+		&storageProtertyQuery, sizeof(storageProtertyQuery), deviceDescriptor, bThousand, NULL, 0)) {
+		printf("%d", GetLastError());
+		CloseHandle(diskHandle);
+		exit(-1);
+	}
+
+	//Выводим свойства диска.
+	cout << "Product ID:    " << (char*)(deviceDescriptor)+deviceDescriptor->ProductIdOffset << endl;
+	cout << "Version        " << (char*)(deviceDescriptor)+deviceDescriptor->ProductRevisionOffset << endl;
+	cout << "Bus type:      " << busType[deviceDescriptor->BusType] << endl;
+	cout << "Serial number: " << (char*)(deviceDescriptor)+deviceDescriptor->SerialNumberOffset << endl;
+}
+
+int main()
+{
+	STORAGE_PROPERTY_QUERY storageProtertyQuery;				//Структура с информацией об запросе
+	storageProtertyQuery.QueryType = PropertyStandardQuery;		//Запрос драйвера, чтобы он вернул дескриптор устройства.
+	storageProtertyQuery.PropertyId = StorageDeviceProperty;	//Флаг, гооврящий мы хотим получить дескриптор устройства.
+
+																//Открытие файла с информацией о диске
+	HANDLE diskHandle = CreateFile("//./PhysicalDrive0", GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ, NULL, OPEN_EXISTING, NULL, NULL);
+	if (diskHandle == INVALID_HANDLE_VALUE) {
+		cout << GetLastError();
+		_getch();
+		return -1;
+	}
+	getDeviceInfo(diskHandle, storageProtertyQuery);
+	_getch();
+	return 0;
+}
