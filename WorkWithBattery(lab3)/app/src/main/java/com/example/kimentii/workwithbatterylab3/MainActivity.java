1c55fad4094dcbf540a119e4cbe045dc1dc9b7c3
Kimentii <malafeikirill@mail.ru>
2017-09-29 19:59:28 +0300

    Добавлено изменение яркости экрана.

--- WorkWithBattery(lab3)/app/src/main/java/com/example/kimentii/workwithbatterylab3/MainActivity.java
+++ WorkWithBattery(lab3)/app/src/main/java/com/example/kimentii/workwithbatterylab3/MainActivity.java
@@ -5,11 +5,13 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.BatteryManager;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
     Button createService;
     Button destroyService;
     TextView textView;
+    SeekBar seekBar;
 
     public class Listener implements View.OnClickListener {
 
@@ -34,6 +37,28 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    public void seekBarInit() {
+        int Brightness;
+        Brightness = Settings.System.getInt(getApplicationContext().getContentResolver(), Settings.System.SCREEN_BRIGHTNESS, 0);
+        seekBar.setProgress(Brightness);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                Settings.System.putInt(getApplicationContext().getContentResolver(), Settings.System.SCREEN_BRIGHTNESS, progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,6 +69,8 @@ public class MainActivity extends AppCompatActivity {
         createService.setOnClickListener(listener);
         destroyService.setOnClickListener(listener);
         textView = (TextView) findViewById(R.id.battary);
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
+        seekBarInit();
         BroadcastReceiver receiver = new BroadcastReceiver() {
             @Override
             public void onReceive(Context context, Intent intent) {
