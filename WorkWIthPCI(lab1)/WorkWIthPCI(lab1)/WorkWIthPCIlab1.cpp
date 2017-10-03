b86cca4a5870391c900b5fc2d17090a4d958616c
Kimentii <malafeikirill@mail.ru>
2017-10-03 09:59:09 +0300

    Добавлен парсинг ven и deм

--- WorkWIthPCI(lab1)/WorkWIthPCI(lab1)/WorkWIthPCIlab1.cpp
+++ WorkWIthPCI(lab1)/WorkWIthPCI(lab1)/WorkWIthPCIlab1.cpp
@@ -14,10 +14,8 @@ using namespace std;
 
 const int BUFFER_SIZE = 350;
 
-int main()
-{
+multimap<string, string> getDeviceList() {
 	HDEVINFO hDevInfo;								//Дескриптор нбора информации об устройстве
-
 	//Получение дескриптора информации об устройсве
 	hDevInfo = SetupDiGetClassDevs(
 		NULL,										//Указатель на класс установки устройства
@@ -30,52 +28,56 @@ int main()
 	{
 		cout << "Error with getting info about devices." << endl;
 		_getch();
-		return 1;
+		return multimap<string, string>();
 	}
 
 	SP_DEVINFO_DATA deviceInfoData;					//Структура с информацией об устройстве
-	
-		//Заполняет структуру devieInfoData
-		DWORD buffersize = BUFFER_SIZE;
 
-		deviceInfoData.cbSize = sizeof(SP_DEVINFO_DATA);	//Установка размера самой структуры.
-		char vendorBuf[BUFFER_SIZE];
-		char deviceBuf[BUFFER_SIZE];
+													//Заполняет структуру devieInfoData
+	DWORD buffersize = BUFFER_SIZE;
 
-		for (int i = 0; SetupDiEnumDeviceInfo(hDevInfo, i, &deviceInfoData); i++) {
-			memset(vendorBuf, 0, BUFFER_SIZE);
-			memset(deviceBuf, 0, BUFFER_SIZE);
-			DWORD DataT;
-			SetupDiGetDeviceRegistryProperty(
-				hDevInfo,									//Информация о наборе устройств
-				&deviceInfoData,							//Информация о том, какое утройство из набора требуется
-				SPDRP_DEVICEDESC,							//Требуемое свойство
-				&DataT,
-				(PBYTE)deviceBuf,							//Буфер
-				BUFFER_SIZE,								//Действительный размер буфера
-				&buffersize);								//Требуемый размер буфера
-			cout << "Devicd ID: ";
-			for (int i = 0; i < BUFFER_SIZE; i++) {
-				if (deviceBuf[i])cout << deviceBuf[i];
+	deviceInfoData.cbSize = sizeof(SP_DEVINFO_DATA);	//Установка размера самой структуры.
+	char buffer[BUFFER_SIZE];
+	regex code("(VEN_)([0-9A-Fa-f]{4})(&DEV_)([0-9A-Fa-f]{4})");
+	multimap<string, string> values;
+	for (int i = 0; SetupDiEnumDeviceInfo(hDevInfo, i, &deviceInfoData); i++) {
+		memset(buffer, 0, BUFFER_SIZE);
+
+		SetupDiGetDeviceRegistryProperty(
+			hDevInfo,									//Информация о наборе устройств
+			&deviceInfoData,							//Информация о том, какое утройство из набора требуется
+			SPDRP_HARDWAREID,							//Требуемое свойство
+			NULL,
+			(PBYTE)buffer,								//Буфер
+			BUFFER_SIZE,								//Действительный размер буфера
+			&buffersize);								//Требуемый размер буфера
+		char info[BUFFER_SIZE];
+		for (int i = 0, j = 0; i < BUFFER_SIZE; i++) {
+			if (buffer[i]) {
+				info[j] = buffer[i];
+				j++;
 			}
-			cout << endl;
-			//Аналогичный вывод vendorID
-			SetupDiGetDeviceRegistryProperty(
-				hDevInfo,
-				&deviceInfoData,
-				SPDRP_MFG,
-				&DataT,
-				(PBYTE)vendorBuf,
-				BUFFER_SIZE,
-				&buffersize);
-			cout << "	Vendor ID: ";
-			for (int i = 0; i < BUFFER_SIZE; i++) {
-				if (vendorBuf[i])cout << vendorBuf[i];
-			}
-			cout << endl;
-			cout << endl;
 		}
-		//Test
+		
+		cmatch result;
+		if (regex_search(info, result, code)) {
+			values.insert(pair<string, string>(string(result[2]), string(result[4])));
+		}
+		cout << endl;
+	}
+	return values;
+}
+
+int main()
+{
+	regex regVendorID("([0-9a-f]{4})( {2})(.*)");
+	regex regDeviceID("(\t)([0-9a-f]{4})( {2})(.*)");
+	multimap<string, string> values = getDeviceList();
+	auto it = values.begin();
+	while (it != values.end()) {
+		cout << it->first << " " << it->second << endl;
+		it++;
+	}
 	system("pause");
     return 0;
 }
