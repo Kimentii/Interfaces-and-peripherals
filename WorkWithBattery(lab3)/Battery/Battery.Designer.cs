d89ec7e720570f5fb702d0c7935fdffd491cfa8d
Kimentii <kmalafey@gmail.com>
2017-10-11 11:18:19 +0300

    Исправлен последний коммит.

---
+++ WorkWithBattery(lab3)/Battery/Battery.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace Battery
+{
+    partial class Battery
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.UpdateTimer = new System.Windows.Forms.Timer(this.components);
+            this.timeoutBox = new System.Windows.Forms.ComboBox();
+            this.timeoutLabel = new System.Windows.Forms.Label();
+            this.timeLeft = new System.Windows.Forms.TextBox();
+            this.Percentage = new System.Windows.Forms.TextBox();
+            this.State = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timeoutBox
+            // 
+            this.timeoutBox.DisplayMember = "300";
+            this.timeoutBox.FormattingEnabled = true;
+            this.timeoutBox.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "5",
+            "10",
+            "15",
+            "20",
+            "25",
+            "30",
+            "45",
+            "60",
+            "120",
+            "180",
+            "240",
+            "300"});
+            this.timeoutBox.Location = new System.Drawing.Point(11, 153);
+            this.timeoutBox.Name = "timeoutBox";
+            this.timeoutBox.Size = new System.Drawing.Size(211, 21);
+            this.timeoutBox.TabIndex = 30;
+            this.timeoutBox.SelectionChangeCommitted += new System.EventHandler(this.timeoutBox_ValueChanged);
+            // 
+            // timeoutLabel
+            // 
+            this.timeoutLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.timeoutLabel.AutoSize = true;
+            this.timeoutLabel.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.timeoutLabel.ForeColor = System.Drawing.Color.Black;
+            this.timeoutLabel.Location = new System.Drawing.Point(8, 135);
+            this.timeoutLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.timeoutLabel.Name = "timeoutLabel";
+            this.timeoutLabel.Size = new System.Drawing.Size(95, 15);
+            this.timeoutLabel.TabIndex = 29;
+            this.timeoutLabel.Text = "Display off time:";
+            // 
+            // timeLeft
+            // 
+            this.timeLeft.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.timeLeft.BackColor = System.Drawing.Color.White;
+            this.timeLeft.Location = new System.Drawing.Point(11, 111);
+            this.timeLeft.Margin = new System.Windows.Forms.Padding(2);
+            this.timeLeft.Name = "timeLeft";
+            this.timeLeft.ReadOnly = true;
+            this.timeLeft.Size = new System.Drawing.Size(211, 20);
+            this.timeLeft.TabIndex = 24;
+            // 
+            // Percentage
+            // 
+            this.Percentage.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.Percentage.BackColor = System.Drawing.Color.White;
+            this.Percentage.Location = new System.Drawing.Point(11, 72);
+            this.Percentage.Margin = new System.Windows.Forms.Padding(2);
+            this.Percentage.Name = "Percentage";
+            this.Percentage.ReadOnly = true;
+            this.Percentage.Size = new System.Drawing.Size(211, 20);
+            this.Percentage.TabIndex = 25;
+            // 
+            // State
+            // 
+            this.State.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.State.BackColor = System.Drawing.Color.White;
+            this.State.Location = new System.Drawing.Point(11, 35);
+            this.State.Margin = new System.Windows.Forms.Padding(2);
+            this.State.Name = "State";
+            this.State.ReadOnly = true;
+            this.State.Size = new System.Drawing.Size(211, 20);
+            this.State.TabIndex = 23;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(8, 57);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 15);
+            this.label2.TabIndex = 27;
+            this.label2.Text = "Status:";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(8, 94);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(105, 15);
+            this.label3.TabIndex = 28;
+            this.label3.Text = "Time to discharge:";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(8, 18);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 15);
+            this.label1.TabIndex = 26;
+            this.label1.Text = "Plugged:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Battery
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(233, 194);
+            this.Controls.Add(this.timeoutBox);
+            this.Controls.Add(this.timeoutLabel);
+            this.Controls.Add(this.timeLeft);
+            this.Controls.Add(this.Percentage);
+            this.Controls.Add(this.State);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Battery";
+            this.Text = "Power Manager";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.AppClosing);
+            this.Load += new System.EventHandler(this.battery_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer UpdateTimer;
+        private System.Windows.Forms.ComboBox timeoutBox;
+        private System.Windows.Forms.Label timeoutLabel;
+        private System.Windows.Forms.TextBox timeLeft;
+        private System.Windows.Forms.TextBox Percentage;
+        private System.Windows.Forms.TextBox State;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
